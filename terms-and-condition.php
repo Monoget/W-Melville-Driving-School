0a4c1d593d824adf9d0eeeafeee0fe95f7cbe89c
Monoget Saha <monoget2@gmail.com>
2025-03-09 01:03:25 +0600

    update all file in php

---
+++ terms-and-condition.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <!-- Required Meta Tags Start -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Melville Driving School</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Required Meta Tags End -->
+
+    <?php include 'include/css.php'; ?>
+</head>
+
+<body>
+
+<?php include 'include/menu.php'; ?>
+
+
+<main>
+    <!-- |==========================================| -->
+    <!-- |=====|| Page Title Start ||===============| -->
+    <section class="pageTitleRs1 pageTitleRs1__bg15">
+        <div class="pageTitleRs1__padding">
+            <div class="container_m_30">
+                <div class="row">
+                    <div class="col-lg-12 col_padding">
+                        <div class="pageTitleRs1__content">
+                            <h1>Service Details</h1>
+                            <div class="pageTitleRs1__bread-crumb">
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="#">Service Details</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- |=====|| Page Title End ||=================| -->
+    <!-- |==========================================| -->
+
+
+    <!-- |==========================================| -->
+    <!-- |=====|| Service Details Start ||===============| -->
+    <section class="serviceDetailsRs1 other_page">
+        <div class="content_box_120">
+            <div class="container_m_30">
+                <div class="row">
+                    <div class="col-xl-4 col_padding order-2 order-xl-0">
+                        <div class="sideBarRs1">
+                            <!-- Search -->
+                            <div class="sideBarRs1__widget mb-50">
+                                <h3>Search Objects</h3>
+                                <div class="sideBarRs1__search">
+                                    <input type="text" placeholder="Search your keyword....">
+                                    <button class="button" type="submit">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <!-- Service List -->
+                            <div class="sideBarRs1__widget mb-50">
+                                <h3>Service List</h3>
+                                <div class="sideBarRs1__categories">
+                                    <ul>
+                                        <li>
+                                            <a class="d-block d-flex justify-content-between" href="#">
+                                                <span>UX/UI Design</span> <span><i
+                                                        class="fas fa-chevron-right"></i></span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="d-block d-flex justify-content-between" href="#">
+                                                <span>Digital Marketing</span> <span><i
+                                                        class="fas fa-chevron-right"></i></span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="d-block d-flex justify-content-between" href="#">
+                                                <span>Web Design</span> <span><i
+                                                        class="fas fa-chevron-right"></i></span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="d-block d-flex justify-content-between" href="#">
+                                                <span>Web Development</span> <span><i
+                                                        class="fas fa-chevron-right"></i></span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="d-block d-flex justify-content-between" href="#">
+                                                <span>Responsive Layout</span> <span><i
+                                                        class="fas fa-chevron-right"></i></span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- Search -->
+                            <div class="sideBarRs1__widget mb-50">
+                                <h3>Have Any Question?</h3>
+                                <div class="sideBarRs1__anyQuestion">
+                                    <form id="contact-form" action="#">
+                                        <div class="row">
+                                            <div class="col-xl-12">
+                                                <input class="form-control" type="text" name="name"
+                                                       placeholder="Full Name...." required>
+                                            </div>
+                                            <div class="col-xl-12">
+                                                <input class="form-control" type="email" name="email"
+                                                       placeholder="Email...." required>
+                                            </div>
+                                            <div class="col-xl-12">
+                                                    <textarea class="form-control" name="message"
+                                                              placeholder="Your Question...." cols="30" rows="4"
+                                                              required></textarea>
+                                            </div>
+                                            <div class="col-xl-12 text-center">
+                                                <div class="main_btn mmt-5">
+                                                    <button type="submit">Send Message</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- Adds -->
+                            <div class="sideBarRs1__widget">
+                                <h3>Sponsor Adds</h3>
+                                <div class="sideBarRs1__add">
+                                    <a href="#"><img src="assets/img/sidebar/sideBar-19.jpg" alt="Side Bar"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col_padding">
+                        <div class="serviceDetailsRs1__content">
+                            <div class="thumbRs1 mb-40">
+                                <img src="assets/img/about/about-25.jpg" alt="Image">
+                            </div>
+                            <h2>Web Development</h2>
+                            <p>Sonsequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+                                fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium
+                                nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto
+                                voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt
+                                quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid.
+                                Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                                sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae
+                                alias error harum maxime adipisci amet laborum. Perspiciatis
+                                minima nesciunt dolorem!</p>
+                            <p>Provide veniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+                                error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+                                error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt
+                                excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci
+                                amet laborum. Perspiciatis minima nesciunt.</p>
+                            <div class="serviceDetailsRs1__thumb1">
+                                <div class="row">
+                                    <div class="col-lg-6 col_padding">
+                                        <p>Voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat
+                                            iusto fuga praesentium optio, eaque rerum! Provident similique
+                                            accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+                                            molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+                                            officia aut!
+                                            Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum
+                                            debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae
+                                            fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt
+                                            excepturi
+                                            expedita sint? Sed quibusdam recusandae alias error harum maxime
+                                            adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Quo neque
+                                            error
+                                            repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi
+                                            at sunt excepturi
+                                            expedita sintasdf a sadf adsf asdfewrtg sedftgre.</p>
+                                    </div>
+                                    <div class="col-lg-6 col_padding">
+                                        <div class="thumbRs1">
+                                            <img src="assets/img/about/about-26.jpg" alt="Image">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>Laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                                optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati
+                                tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet
+                                aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+                                nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error
+                                repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt
+                                excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci
+                                amet laborum. Perspiciatis
+                                minima nesciunt dolorem!</p>
+                            <h3>Key for your brand communication</h3>
+                            <p>Sonsequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+                                fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium
+                                nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto
+                                voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt
+                                quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid.
+                                Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                                sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae
+                                alias error harum maxime adipisci amet laborum. Perspiciatis
+                                minima nesciunt dolorem!</p>
+                            <p>Provide veniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+                                error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+                                error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt
+                                excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci
+                                amet laborum. Perspiciatis minima nesciunt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- |=====|| Service Details End ||=================| -->
+    <!-- |==========================================| -->
+</main>
+
+
+<?php include 'include/footer.php'; ?>
+
+
+<?php include 'include/js.php'; ?>
+
+</body>
+
+</html>
